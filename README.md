4026199004eb0274bf9fb55a8a7107c975b56fa1
takikuba <jakub26@gmail.com>
2020-11-25 11:19:31 +0100

    Rebuild project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# WDPAI" 
+#***Projects for WDPAI***
+
+Projekt aplikacji internetowej
+"Make Eat Better"
+
+## **Cel**
+Celem aplikacji jest pomoc osobą średnio uzdolnionym kulinarnie w stworzenia idealnego posiłku, biorąc pod uwagę nie tylko smak ale również czas, składniki i ilość kalorii.
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'DefaultController');
+Router::post('login', 'SecurityController');
+
+Router::run($path);
